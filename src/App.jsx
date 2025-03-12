9c1f4f378a8d4c00595680151ad73e7db342ae89
Pawanreddy <ponereddy@gmail.com>
2025-03-12 21:34:00 +0530

    adding the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,26 @@
 import { useState } from 'react'
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
+import Body from './Components/body/body';
+import Login from './Components/login/login';
+import Profile from './Components/Profile/Profile';
 
 function App() {
   
 
   return (
-    <>      
-     <div className="navbar bg-base-100 shadow-sm">
-  <div className="flex-1">
-    <a className="btn btn-ghost text-xl">Pawan Store</a>
-  </div>
-  <div className="flex gap-2">
-    <div className="dropdown dropdown-end">
-      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-        <div className="w-10 rounded-full">
-          <img
-            alt="Tailwind CSS Navbar component"
-            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-        </div>
-      </div>
-      <ul
-        tabIndex={0}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li>
-          <a className="justify-between">
-            Profile
-            <span className="badge">New</span>
-          </a>
-        </li>
-        <li><a>Settings</a></li>
-        <li><a>Logout</a></li>
-      </ul>
-    </div>
-  </div>
-</div>
+    <>    
+    <BrowserRouter basename='/'>
+      <Routes >
+              <Route path='/' element={<Body></Body>}>
+                  <Route path='/login' element={<Login></Login>}></Route>
+                  <Route path='Profile' element={<Profile></Profile>}></Route>
+              </Route>
+             
+             
+      </Routes>
+    </BrowserRouter>  
     </>
   )
 }
